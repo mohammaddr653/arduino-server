e06fc1580321cb2a9a3a493b6acf15045ff95897
Ozone <mohammaddr653@gmail.com>
2025-09-21 17:36:24 +0330

    v4-created static paths

--- back-end/index.js
+++ back-end/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..", "client")));
+app.use("/public", express.static(path.join(__dirname, "..", "public"))); //چون dirname مسیر همین فایل رو نشون میده پس باید اول یک پوشه به عقب بره تا برسه به public
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 const array = [];
 
